d16218f777e01feb3ef33da11a5cce6e5e26b8e6
bakpobatombari <batombari12@gmail.com>
2024-07-28 22:02:39 +0100

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,7 +31,7 @@ function myFunction() {
     }
   }
   // Set the date we're counting down to
-var countDownDate = new Date("july 24, 2024 10:00:00").getTime();
+var countDownDate = new Date("july 28, 2024 9:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
